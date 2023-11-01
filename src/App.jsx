1e9e8a58ce54ef764c9989e0a4df878e3faa82cb
marko <marko@marko.com>
2023-11-01 19:09:14 +0100

    finished Checkout cmp

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Cart from "./components/Cart/Cart";
+import Checkout from "./components/Cart/Checkout";
 import Meals from "./components/Meals/Meals";
 import Header from "./components/layout/Header";
 import Layout from "./components/layout/Layout";
@@ -12,6 +13,7 @@ function App() {
         <Layout />
         <Meals />
         <Cart />
+        <Checkout />
       </CartProvider>
     </UserProgressContexProvider>
   );
--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -2,20 +2,41 @@ import React, { useContext } from "react";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
 import { currencyFormatter } from "../../util/formatting";
+import Input from "../UI/Input";
+import Button from "../UI/Button";
+import UserProgressContex from "../../store/UserProgressContex";
 
 function Checkout() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContex);
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
 
+  const handleClose = () => {
+    userProgressCtx.hideCheckout();
+  }
+
   return (
-    <Modal>
+    <Modal open={userProgressCtx.hideCart === "checkout"}>
       <form>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="E-mail address" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+        <p className="modal-actions">
+          <Button onClick = {handleClose} type="button" textOnly>
+            Close
+          </Button>
+          <Button>Submit Order</Button>
+        </p>
       </form>
     </Modal>
   );
